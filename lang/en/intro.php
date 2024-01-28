912df9f993052c6f8569dbbb866c26a9fdbdbbae
rayesgulfnetwork <155744887+rayesgulfnetwork@users.noreply.github.com>
2024-01-29 03:06:53 +0530

    language setup

---
+++ lang/en/intro.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+	'intro_title' => 'A brief introduction to Darul Uloom Ahl-e-Sunnat Gharib Nawaz',
+	'intro1' => 'Darul Uloom Ahl-e-Sunnat Gharib Nawaz stands as a distinguished religious seminary
+in the northeastern region of Uttar Pradesh. It serves as a sanctuary where the
+propagation of beliefs and religions is carried out without any hindrance, akin to a
+precious gem held securely in the hands of its people. Since its establishment, the
+institution has rendered commendable and unforgettable services, earning recognition
+across India and the Indian subcontinent. The voices of Allah and the Messenger of
+Allah resonate from this institution, enlightening the minds of the people.',
+	
+'intro2' => 'The architectural allure of this institution is captivating, extending a warm invitation to
+every visitor. It functions as a hub where proficient and seasoned teachers and scholars
+nourish various sciences. From here, beliefs and practices are reformed, transforming
+individuals into genuine followers of the Sunnah and the esteemed Prophet. The
+institution has, in a relatively short period (nearly 24 years), attained remarkable heights
+and perfection, carving out its own identity nationally and internationally. This success is
+attributed to continuous efforts, unwavering dedication, and the persistent pursuit of
+religious excellence.',
+
+
+'intro3' => 'The institution plays a vital role in raising awareness about beliefs and imparting the
+teachings of Huqa, serving as the authentic representative of Imam Abu Hanifa&#39;s
+doctrine and the sect of the revered Hazrat in the Domariaganj area. Such
+achievements are the outcomes of the tireless endeavors inspired by the Holy Prophet
+and the Ghareeb Nawaz Hazrat Khwaja Moinuddin Chishti Ajmer (May Allah be pleased
+with him). The leadership of the Nation&#39;s Khalifah, Hazrat Ajmal Ulama, and the Khalifah
+Huzur Faqih-e-Millat (Alaihi Rahma), Hazrat Allama Al-Hajj Muhammad Hafeezullah
+Qadri Ashrafi, has played a pivotal role in the institution&#39;s continuous development.',
+
+'intro4' => 'To this day, the institution has discharged its duties with grace, honesty, good manners,
+and a missionary spirit. It remains committed to constant activity and effort for its
+ongoing progress and enhancement.',
+
+'intro5' => 'May Allah Almighty elevate your ranks, safeguard this institution from the evil
+eyes of the envious and the wicked, and provide hidden means for its further
+development. (Ameen, by the blessings of the Leader of the Prophets, peace and
+blessings be upon him.',
+
+
+];
+
+?>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -10,22 +10,19 @@
 	<div class="card-body">
 		<div class="container">
 			<div class="row">
-			<div class="col-md-9 col-12">
-				<h2 class="title mt-6">A brief Introduction to Darul Uloom Ahle Sunnat Gareeb Nawaz & Technical Institute Baidaula</h2>
-				<p> For the benefit of man kind and Islamic followers and its flourishing the education and schooling of the new generation is the basic need of the hour. This noble object has always drawn the attention of Imam Ahmad Rida Khan and Mufti Azam al-Hind and after them, Qadhi al-Qudhat Taaj al-Shari'ah Shaykh Akhtar Rida Khan al-Azhari to establish the grand religious institution that could be conductive to profound learning and the thorough correct understanding of the Holy Qur'an and Sunnah, so as to discover the hidden part of Islamic ideology to suit the present situations in the scientific and technological era. And to inculcate into the new generation, the true Islamic spirit and characteristics of a devoted preacher.
-				</p>
-				<p>
-					At the same time it may emphatically gird them up with modern knowledge, so that they could safeguard themselves against heretical ideologies and material outlook and with their reasonable and logical approach they could put forth Islam to others, as an ideal and evergreen way of life and beneficial to mankind.
-				</p>
-				<p>
-					The 20th century's great Islamic reviver, the Imam Ahmad Rida Khan, is the only thinker who brought an awakening in the Muslim community and provided the safe guard for Islam from the different seductions of the era.Sub serving this ideology, Taaj al-Shar'iah felt a strong urge to lay down an effective system of education for the Sunni Muslims, which may infuse into our learners orthodox as well as essential modern branches of knowledge.Hence keeping the above view, the Centre of Islamic Studies Jamiat-ur-Raza was instituted on the 29th day of May 2000 and from the very beginning it has been following this aims and objectives.
-				</p>
+			<div class="col-md-12 col-12">
+				<h2 class="title mt-6">@lang('intro.intro_title')</h2>
+				<p> @lang('intro.intro1') </p>
+				<p> @lang('intro.intro2') </p>
+				<p>@lang('intro.intro3')</p>
+				<p>@lang('intro.intro4')</p>
+				<p>@lang('intro.intro5')</p>
 			</div>
-			<div class="col-md-3 col-12">
+			<!-- <div class="col-md-3 col-12">
 				<figure class="product-gallery-image">
 					<img src="{{ asset('frontend/assets/logo/logo.jpg') }}" alt="who we are image">
 				</figure><!-- End .product-gallery-image -->
-			</div>
+			</div> -->
 		</div>
 		</div>		
 	</div>
